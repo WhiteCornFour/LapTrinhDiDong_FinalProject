a025b954d0e006b2f67e8e8d0bb0bb9d6bb79017
Nevermind <137763987+lethanhphat1005@users.noreply.github.com>
2024-06-24 13:24:48 +0700

    updatenhanhphat

--- app/src/main/java/com/example/laptrinhdidong_finalproject/Activity_Login_Customer.java
+++ app/src/main/java/com/example/laptrinhdidong_finalproject/Activity_Login_Customer.java
@@ -11,12 +11,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Activity_Login_Customer extends AppCompatActivity {
 
     EditText edtPasswordLogin, edtFullNameLogin;
     Button btnLoginCustomer;
 
     TextView tvRegisterLogin, tvForgetPasswordLogin, tvLoginAD;
+
+    String filename = "register";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +51,29 @@ public class Activity_Login_Customer extends AppCompatActivity {
         btnLoginCustomer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Activity_Login_Customer.this, MainActivity.class);
-                intent.putExtra("fullname", edtFullNameLogin.getText().toString());
-                intent.putExtra("password", edtPasswordLogin.getText().toString());
-                startActivity(intent);
+                String fullName = edtFullNameLogin.getText().toString();
+                String password = edtPasswordLogin.getText().toString();
+
+//                if (!validateInputs(fullName, password)) {
+//                    return;
+//                }
+
+                boolean isValid = false;
+                try {
+                    isValid = validateLoginInfo(filename, fullName, password);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                if (isValid) {
+                    Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Activity_Login_Customer.this, MainActivity.class);
+                    intent.putExtra("fullname", fullName);
+                    intent.putExtra("password", password);
+                    startActivity(intent);
+                    resetEdt();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Invalid username or password", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -78,11 +101,48 @@ public class Activity_Login_Customer extends AppCompatActivity {
         });
     }
 
+    boolean validateLoginInfo(String filename, String inputUsername, String inputPassword) throws IOException {
+        InputStream inputStream = openFileInput(filename);
+        int size = inputStream.available();
+        byte[] data = new byte[size];
+        inputStream.read(data);
+        String kq = new String(data);
+        inputStream.close();
+
+        String[] dstaikhoan = kq.split("==");
+        for (String s : dstaikhoan) {
+            String[] thongTinTaiKhoan = s.split(";;");
+            if (thongTinTaiKhoan.length >= 2) {
+                String username = thongTinTaiKhoan[0].trim();
+                String password = thongTinTaiKhoan[1].trim();
+                if (username.equals(inputUsername) && password.equals(inputPassword)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean validateInputs(String username, String password) {
+        // Kiểm tra username có hơn 8 ký tự
+        if (username.trim().isEmpty() || username.trim().length() <= 8) {
+            Toast.makeText(this, "Username phải có hơn 8 ký tự", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        // Kiểm tra password có hơn 8 ký tự
+        if (password.trim().isEmpty() || password.trim().length() <= 8) {
+            Toast.makeText(this, "Password phải có hơn 8 ký tự", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
         //---------------------------
-        SharedPreferences sharedPreferences =getPreferences(MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
         String fn = edtFullNameLogin.getText().toString();
@@ -90,6 +150,11 @@ public class Activity_Login_Customer extends AppCompatActivity {
 
         editor.putString("tên đầy đủ", fn);
         editor.putString("mật khẩu", ps);
+        editor.apply();
+    }
 
+    void resetEdt() {
+        edtFullNameLogin.setText("");
+        edtPasswordLogin.setText("");
     }
 }
